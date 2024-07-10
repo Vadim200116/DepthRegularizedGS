c9896d1b98e3caa8df793d095c40f974b5162e4b
Vadim Pryadilshchikov <vadim-pr2001@mail.ru>
2024-07-10 14:03:39 +0300

    reg depth diff by tv

--- train.py
+++ train.py
@@ -112,12 +112,6 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         Ll1 = l1_loss(image, gt_image, mask)
         loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image, mask=mask))
 
-
-        if opt.lambda_tv and iteration > opt.tv_from_iter and iteration < opt.tv_until_iter:
-            depth = normalize_depth(render_pkg["depth"])
-            tv = total_variation_loss(depth)
-            loss += opt.lambda_tv * tv
-
         ### depth supervised loss
         depth = render_pkg["depth"]
         if usedepth and viewpoint_cam.original_depth is not None:
@@ -129,6 +123,11 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
             deploss = l1_loss(gt_maskeddepth, depth*depth_mask, mask) * opt.lambda_depth
             loss = loss + deploss
+
+            if opt.lambda_tv:
+                diff = depth*depth_mask - gt_maskeddepth
+                tv = total_variation_loss(diff, mask)
+                loss += opt.lambda_tv * tv
 
         ## depth regularization loss (canny)
         if usedepthReg and iteration>=0: 
